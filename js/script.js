4ac4198d289efa66c158ac65cc1c74840feb1318
Jakub Sadowski <kuba.sadowsky@gmail.com>
2024-01-10 13:06:21 +0100

    Add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 'use strict'; //kod będzie uruchamiany w "trybie ścisłym" - pomyłki, które normalnie nie wywołałyby błędu, teraz będą traktowane jak błąd i wyświetlane na czerwono
 
-function titleClickHandler(event){ //funkcja, która jest wykonywana w reakcji na event (kliknięcie na link)
+function titleClickHandler(event){ //funkcja, która jest wykonywana w reakcji na event (kliknięcie na link); w argumencie "event" można znaleźć m.in. informacje "target", która zawiera odniesienie do <span>
+    const clickedElement = this; //obiekt "this" wskazuje na element, do którego dodaliśmy listener, czyli <a>
     console.log('Link was clicked!');
-    console.log(event);
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
-
-    /* remove class 'active' from all articles */
+    /* [DONE] add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.post.active');
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
@@ -21,6 +22,7 @@ function titleClickHandler(event){ //funkcja, która jest wykonywana w reakcji n
 
     /* add class 'active' to the correct article */
 }
+
 const links = document.querySelectorAll('.titles a'); //przypisanie do stałej "links" wszystkich elementów, pasujących do selektora ".titles a"
 
 for(let link of links){ //przypisanie "event listenerów" do każdego linka za pomocą pętli
