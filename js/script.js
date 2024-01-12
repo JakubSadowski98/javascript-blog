0d5fb2e61a60462389daef21ab45ae856db158e9
Jakub Sadowski <kuba.sadowsky@gmail.com>
2024-01-12 18:44:34 +0100

    Generate tags for every article; Add action after tag click

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ function titleClickHandler(event){ //funkcja, która jest wykonywana w reakcji n
   console.log('Link was clicked: function titleClickHandler() was run');
   event.preventDefault(); //blokuje mechanizm automatycznego przewijania strony w miejsce wskazanego przez link id (np. do artykułu) oraz zmiany adresu url w skutek kliknięcia na link
   const clickedElement = this; //obiekt "this" wskazuje na element, do którego dodaliśmy "event listener", czyli na link <a>
-  /* ALGORITHM STEPS */
   /* 1. Remove class 'active' from all article links [DONE] */
   const activeLinks = document.querySelectorAll('.titles a.active');
   for(let activeLink of activeLinks){
@@ -19,7 +18,6 @@ function titleClickHandler(event){ //funkcja, która jest wykonywana w reakcji n
   }
   /* 2. Add class 'active' to the clicked link [DONE] */
   clickedElement.classList.add('active');
-  console.log('ClickedElement: ', clickedElement);
   /* 3. Remove class 'active' from all articles [DONE] */
   const activeArticles = document.querySelectorAll('.posts article.post.active');
   for(let activeArticle of activeArticles){
@@ -27,10 +25,8 @@ function titleClickHandler(event){ //funkcja, która jest wykonywana w reakcji n
   }
   /* 4. Get 'href' attribute from the clicked link [DONE] */
   const articleSelector = clickedElement.getAttribute('href'); //przypisanie do stałej "articleSelector" wartości atrybutu "href" pobranej z klikniętego linka
-  console.log('Href s value was assigned to articleSelector: ', articleSelector);
   /* 5. Find the correct article using the selector (href s value) [DONE] */
   const targetArticle = document.querySelector(articleSelector); //przypisanie elementu do stałej "targetArticle" za pomocą funkcji "querySelector", wyszukującej artykuł o danej wartości atrybutu href (zapisanej w stałej "articleSelector")
-  console.log('Selected Article: ',targetArticle);
   /* 6. Add class 'active' to the correct article [DONE] */
   targetArticle.classList.add('active');
   console.log('Right article was displayed');
@@ -38,7 +34,6 @@ function titleClickHandler(event){ //funkcja, która jest wykonywana w reakcji n
 
 /* Generate list of title-links */
 function generateTitleLinks(){
-  /* ALGORITHM STEPS */
   /* 1. Remove content of titleList [DONE] */
   const titleList = document.querySelector(optTitleListSelector); //znalezienie listy linków i przypisanie jej do stałej "titleList"
   function clearMessages(){ //funkcja usuwająca zawartość elementu "titleList" - usuwa wszystkie linki
@@ -47,7 +42,7 @@ function generateTitleLinks(){
   clearMessages(); //wywołanie funkcji
 
   /* for each article */
-  const articles = document.querySelectorAll(optArticleSelector); //zapisanie do stałej "articles" odniesienia do wszystkich artykułów
+  const articles = document.querySelectorAll(optArticleSelector); //znalezienie wszystkich artykułów i zapisanie ich do stałej "articles"
   let html = ''; //stworzenie zmiennej (nie stałej!) "html", w której zapiszemy linijki kodu HTML dla linków
   for(let article of articles){ //utworznie pętli do wykonania pozostałych operacji z osobna dla każdego z artykułów
     /* 2. Get the article id [DONE] */
@@ -74,7 +69,6 @@ for(let link of links){ //przypisanie "event listenerów" do każdego linka zawi
 
 /* Generate tags for every article */
 function generateTags(){
-  /* ALGORITHM STEPS */
   /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector); //stała "articles" jest kolekcją wielu elementów, które zawierają odniesienie do każdego artykułu
   /* START LOOP: for every article: */
@@ -89,16 +83,57 @@ function generateTags(){
     const articleTagsArray = articleTags.split(' '); //utworzono tablicę - funkcja ".split(' ')" usunie spacje oraz upodzieli tekst zawarty w "articleTags" na osobne elementy, które od teraz będą zawarte w tablicy "articleTagsArray"
     /* START LOOP: for each tag */
     for(let tag of articleTagsArray){ //wewnątrz pętli zmienna "tag" będzie treścią pojedynczego taga
-    /* generate HTML of the link */
+      /* generate HTML of the link */
       const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
-    /* add generated code to html variable */
+      /* add generated code to html variable */
       html = html + linkHTML;
-      console.log(html);
-    /* END LOOP: for each tag */
-    }
+    }/* END LOOP: for each tag */
     /* insert HTML of all the links into the tags wrapper */
     tagsWrapper.innerHTML = html;
     /* END LOOP: for every article: */
   }
 }
+
 generateTags();
+
+/* Add action after tag click */
+function tagClickHandler(event){
+  /* prevent default action for this event [DONE] */
+  event.preventDefault();
+  /* make new constant named "clickedElement" and give it the value of "this" [DONE] */
+  const clickedElement = this;
+  /* make a new constant "href" and read the attribute "href" of the clicked element [DONE] */
+  const href = clickedElement.getAttribute('href');
+  /* make a new constant "tag" and extract (wyodrębnij) tag from the "href" constant [DONE] */
+  const tag = href.replace('#tag-', ''); //funkcja "replace" otrzymuje dwa argumenty – szukaną frazę oraz ciąg znaków, którym ma ją zastąpić (dla href = '#tag-cat' otrzymamy samo 'cat')
+  /* find all tag links with class active [DONE] */
+  const activeTagLinks = document.querySelectorAll('a.active[href^="#tag-"]'); //znajduje wszystkie aktywne linki tagów z wykorzystaniem selektora atrybutu; łącznik ^= , oznacza "atrybut href zaczynający się od '#tag-'"
+
+  for(let activeTagLink of activeTagLinks){ /* START LOOP: for each active tag link */
+    /* remove class active [DONE] */
+    activeTagLink.classList.remove('active');
+  }/* END LOOP: for each active tag link */
+
+  /* find all tag links with "href" attribute equal to the "href" constant [DONE] */
+  const tagLinks = document.querySelectorAll('a[href="' + href + '"]'); //znajduje wszystkie linki tagów z wykorzystaniem selektora atrybutu, które mają taki sam atrybut href, jak kliknięty link
+
+  for(let tagLink of tagLinks){/* START LOOP: for each found tag link */
+    /* add class active [DONE] */
+    tagLink.classList.add('active');
+  }/* END LOOP: for each found tag link */
+
+  /* execute (wywołaj) function "generateTitleLinks" with article selector as argument */
+  generateTitleLinks('[data-tags~="' + tag + '"]'); //łącznik ~= można odczytać jako "znajdź elementy, które mają atrybut data-tags, który ma w sobie słowo 'tag'".
+}
+
+function addClickListenersToTags(){
+  /* find all links to tags */
+
+  /* START LOOP: for each link */
+
+    /* add tagClickHandler as event listener for that link */
+
+  /* END LOOP: for each link */
+}
+
+addClickListenersToTags();
